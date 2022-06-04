4e210cbaa1affbe119becd75732771198ebc0201
Tanvi Kayat <105040357+TKayat@users.noreply.github.com>
2022-06-04 22:30:33 +0530

    Add files via upload

---
+++ Admin/JobList.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineJobPortal.Admin
+{
+    public partial class JobList : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        DataTable dt;
+
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        string query;
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (Session["admin"] == null)
+            {
+                Response.Redirect("../User/Login.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+                ShowJob();
+            }
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowJob();
+        }
+
+        private void ShowJob()
+        {
+            query = string.Empty;
+            con = new SqlConnection(str);
+            query = @"Select Row_Number() over(Order by (Select 1)) as [Sr.No], JobId,Title,NoOfPost,Description,Qualification,Experience,
+                        LastDatetoApply,CompanyName,Country,State,CreateDate from Jobs";
+            cmd = new SqlCommand(query, con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            //throw new NotImplementedException();
+        }
+
+        protected void GridView1_OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            ShowJob();
+
+        }
+
+        protected void GridView1_OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {   
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int jobId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                con = new SqlConnection(str);
+                cmd = new SqlCommand("Delete from Jobs where JobId=@id", con);
+                cmd.Parameters.AddWithValue("@id", jobId);
+                con.Open();
+                int r = cmd.ExecuteNonQuery();
+                if (r > 0)
+                {
+                    lblMsg.Text = "Job Delete Successfully!";
+                    lblMsg.CssClass = "alert alert-success";
+
+                }
+                else
+                {
+                    lblMsg.Text = "Cannot delete this record!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                con.Close();
+                GridView1.EditIndex = -1;
+                ShowJob();
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+
+            }
+
+        }
+
+        protected void GridView1_OnRowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditJob")
+            {
+                Response.Redirect("NewJob.aspx?id="+e.CommandArgument.ToString());
+            }
+
+        }
+    }
+}
